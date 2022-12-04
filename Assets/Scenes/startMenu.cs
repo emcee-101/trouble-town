57f2ae8920c4ef482faf0b7c52e3c2329aa239a0
DEVmozi <zid.m@pm.me>
2022-12-04 19:10:25 +0100

    Bugfix: Mouse crosshair now invisible and locked.

--- Assets/Scenes/startMenu.cs
+++ Assets/Scenes/startMenu.cs
@@ -8,6 +8,10 @@ public class startMenu : MonoBehaviour
     
    public void startGame()
     {
+        // Make Mouse crosshair invisible and locked inside the game window
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+
         SceneManager.LoadScene(1);
     }
 
