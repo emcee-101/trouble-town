b6c9d7ab135a94ebb7da03c16d94fb7b52889046
Nycelinux <celina.ludwigs@fh-erfurt.de>
2023-02-23 15:35:25 +0100

    Endui script, lose,win and Panel prefab added

---
+++ Assets/Prefabs/Player/endUI.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class endUI : MonoBehaviour
+{
+    public GameObject winPanel;
+    public GameObject losePanel;
+
+    Init();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
