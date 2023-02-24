1cdb5cc67d1f04736c0c06599a876dcf928633c1
Niklas Herzog <nik.las.herzog@t-online.de>
2023-02-25 00:35:54 +0100

    what the branch name says

---
+++ Assets/Scripts/UI/endUI/endUI.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class endUI : MonoBehaviour
+{
+    public GameObject winPanel;
+    public GameObject losePanel;
+
+    public void showEndUIValues(bool hasWon, bool policeWon, bool gameEnded)
+    {
+
+        // do something
+
+    }
+}
