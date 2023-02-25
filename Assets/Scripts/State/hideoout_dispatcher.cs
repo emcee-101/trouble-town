563b78a4a57e33cf558c9ef51ffada66cfdad2cf
Niklas Herzog <nik.las.herzog@t-online.de>
2023-02-25 14:57:28 +0100

    added OwnHideouts Feature

---
+++ Assets/Scripts/State/hideoout_dispatcher.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class hideoout_dispatcher : MonoBehaviour
+{
+    public GameObject[] hideouts;
+    public int numOfHideouts;
+    public List<Transform> hideoutPositions;
+    public GameObject prefabForHideout;
+    public GameObject parentObj;
+
+    void Start()
+    {
+        hideouts = GameObject.FindGameObjectsWithTag("Hideout");
+
+        numOfHideouts = hideouts.Count();
+        hideoutPositions = new List<Transform>();
+
+        foreach(GameObject hideout in hideouts)
+        {
+            hideoutPositions.Add(hideout.transform);
+            hideout.SetActive(false);
+
+        }
+
+
+    }
+
+    public GameObject dispatchHideout(int number) {
+
+        if (number <= numOfHideouts)
+        {
+            
+                                                    // minus 1 for the fact that player 1 is the police guy and minus 1 for the array/list beginning at 0
+            return Instantiate(prefabForHideout, hideoutPositions[number - 1 - 1].position, hideoutPositions[number  - 1 - 1].rotation, parentObj.transform);
+           
+
+        }
+        else return null;
+    }
+
+}
