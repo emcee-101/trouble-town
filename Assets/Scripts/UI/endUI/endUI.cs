b5977006f5481cbbae3580640169e6ee65b33d3b
Nycelinux <celina.ludwigs@fh-erfurt.de>
2023-02-25 12:02:27 +0100

    Script hasnot errors

--- Assets/Scripts/UI/endUI/endUI.cs
+++ Assets/Scripts/UI/endUI/endUI.cs
@@ -6,11 +6,43 @@ public class endUI : MonoBehaviour
 {
     public GameObject winPanel;
     public GameObject losePanel;
+    public GameObject Panel;
 
     public void showEndUIValues(bool hasWon, bool policeWon, bool gameEnded)
     {
-
+        winPanel = GameObject.Find("winPanel");
+        losePanel = GameObject.Find("losePanel");
+        Panel = GameObject.Find("Panel");
         // do something
+        if (gameEnded==true)
+      {
+            
+
+            if (policeWon == true)
+            {
+                //Panel = GameObject.Find("winPanel");
+
+                Panel=winPanel;
+            }
+            else if (policeWon == false)
+            {
+                //Panel=GameObject.Find("losePanel");
+                Panel= losePanel;
+            }
+            else if (hasWon == true)
+            {
+                //Panel = GameObject.Find("winPanel");
+                Panel= winPanel;
+
+            }
+            else
+            {
+                //Panel = GameObject.Find("losePanel");
+                Panel=losePanel;
+            }
+      }  
+        
 
     }
 }
+
